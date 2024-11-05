36f72fc63f5beb9e6788456d8470dc956a97a3d4
sangu272 <sangeeta271713@gmail.com>
2024-11-05 22:50:45 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -42,9 +42,9 @@ def welcomepic(pic, user, chat, id, uname):
     draw = ImageDraw.Draw(background)
     font = ImageFont.truetype('assets/font.ttf', size=60)
     font2 = ImageFont.truetype('assets/font.ttf', size=90)
-    draw.text((610, 300), f'NAME : {unidecode(user)}', fill="white", font=font)
+    draw.text((600, 300), f'NAME : {unidecode(user)}', fill="white", font=font)
     draw.text((650, 450), f'ID : {id}', fill="white", font=font)
-    draw.text((7000, 600), f"USERNAME : {uname}", fill="white",font=font)
+    draw.text((700, 600), f"USERNAME : {uname}", fill="white",font=font)
     pfp_position = (100, 133)  
     background.paste(pfp, pfp_position, pfp)  
     background.save(

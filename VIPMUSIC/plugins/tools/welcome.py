6357ddbc4b7ba030223365b69ac8ee0eab3131b0
sangu272 <sangeeta271713@gmail.com>
2024-11-05 19:50:19 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -96,18 +96,31 @@ def circle(pfp, size=(900, 900), brightness_factor=10):
     pfp.putalpha(mask)
     return pfp
 
-def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
+def welcomepic(user_id, user_username, user_names, chat_name, user_photo, chat_photo):
     background = Image.open("assets/wel2.png")
-    pfp = Image.open(pic).convert("RGBA")
-    pfp = circle(pfp, brightness_factor=brightness_factor) 
-    pfp = pfp.resize((460, 450))
+    user_img = Image.open(user_photo).convert("RGBA")
+    chat_img = Image.open(chat_photo).convert("RGBA")
+    
+    chat_img_circle = circle(chat_img, size=(240, 240), brightness_factor=1.2)
+    user_img_circle = circle(user_img, size=(232, 232), brightness_factor=1.2)
+    
+    background.paste(chat_img_circle, (270, 260), chat_img_circle)
+    background.paste(user_img_circle, (827, 260), user_img_circle)
+    
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype('assets/font.ttf', size=110)
-    welcome_font = ImageFont.truetype('assets/font.ttf', size=60)
-    pfp_position = (135, 130)
-    background.paste(pfp, pfp_position, pfp)
-    background.save(f"downloads/welcome#{id}.png")
-    return f"downloads/welcome#{id}.png"
+    font = ImageFont.truetype("assets/font.ttf", size=32)
+
+  
+    saffron = (255, 153, 51)  
+    white = (255, 255, 255)   
+    green = (19, 136, 8)
+
+    draw.text((510, 517), f"Name:  {user_names}", fill=saffron, font=font)
+    draw.text((510, 547), f"User Id:  {user_id}", fill=white, font=font)
+    draw.text((510, 580), f"Username:  {user_username}", fill=green, font=font)
+    
+    background.save(f"downloads/welcome#{user_id}.png")
+    return f"downloads/welcome#{user_id}.png
 
 
 @app.on_message(filters.command("welcome") & ~filters.private)
